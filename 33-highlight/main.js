bdbfe05453cb2aadd11868a0c591ae6388949e91
pankaiya <3131225624@qq.com>
2019-04-04 10:06:20 +0800

    语法高亮highlight

--- 33-highlight/main.js
+++ 33-highlight/main.js
@@ -1,11 +1,52 @@
-/* global hljs:true */
+/* global Behave hljs: true */
 $(function(){
-   var $code = $('input[type="button"]');
-  
-   $btnAddCode.click(function(){
-     var $code = $('<div><pre class="javascript></pre></div>"');
+  var editor = new Behave({
+      textarea: $('.main textarea').get(0),
+      replaceTab: true,
+      softTabs: true,
+      tabSize: 2,
+      autoOpen: true,
+      overwrite: true,
+      autoStrip: true,
+      autoIndent: true,
+      fence: false
+    });
 
-     $code.find('pre').html($('.code').val());
-   })
-  hljs
- });
+  $('input[type="button"]').click(function() {
+    var $code = $(''
+        + '<div>'
+          + '<pre class="javascript">'
+          + '</pre>'
+          + '<i class="iconfont icon-clipboard"></i>'
+          + '<i class="iconfont icon-delete"></i>'
+         + '</div>'),
+        $txt  = $('.main textarea');
+
+    if($txt.val() !== '') {
+      $code.find('pre').html($txt.val());
+      hljs.highlightBlock($code.find('pre').get(0));
+      $('.main').append($code);
+    }
+
+    var $del = $code.find('i.icon-delete'),
+        $copy = $code.find('i.icon-clipboard');
+
+    $del.click(function() {
+      $code.remove();
+    });
+
+    $copy.click(function() {
+      var $content = $code.find('pre');
+
+      var range = document.createRange();
+      range.selectNode($content[0]);
+      window.getSelection().addRange(range);
+
+      document.execCommand('copy');
+      window.getSelection().removeAllRanges();
+     
+   });
+  });
+    return editor;
+});
+
